060df2271bd5ca42301a287c99653f90b5faaad2
Marco F <marco.friso@gmail.com>
2017-10-12 23:11:18 +0200

    deck branch

---
+++ php/deck.php
@@ -0,0 +1,32 @@
+<?php
+include_once 'array.php';
+
+//echo $card;
+//$list = $card;
+//var_dump($list);
+
+//echo(spl_object_hash($card));
+
+print_r($card);
+
+?>
+
+
+
+
+<!--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/spades.css">
+</head>
+<body>
+    <?php //echo $card;?>
+    
+</body>
+</html>
+-->
